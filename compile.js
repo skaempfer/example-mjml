098bb97e267caf8bd6b539116c6022e8ec182879
Sebastian Kämpfer <sebastian.kaempfer@arvato-scs.com>
2021-08-03 15:48:53 +0200

    Renaming

--- compile.js
+++ compile.js
@@ -3,7 +3,7 @@ const handlebars = require('handlebars');
 const fs = require('fs');
 const data = require('./data.json');
 
-const mjmlContent = fs.readFileSync('03-index.mjml', 'utf-8');
+const mjmlContent = fs.readFileSync('03-example.mjml', 'utf-8');
 
 const mjmlResult = mjml2html(mjmlContent);
 
